0b61d3c8c5f977a6baae05c586f63bc4557c3ba5
Konstantin Razvodovskiy <hfprjc@gmail.com>
2020-07-06 16:45:12 +0700

    [+] Стартовый Коммит

--- app/src/main/java/com/example/myapplication/MenuActivity.kt
+++ app/src/main/java/com/example/myapplication/MenuActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
-import kotlinx.android.synthetic.main.activity_menu.textview_random
-import kotlinx.android.synthetic.main.activity_menu.textview_label
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.util.*
@@ -15,23 +14,5 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        showRandomNumber()
-    }
-    fun showRandomNumber() {
-        // Get the count from the intent extras
-        val count = intent.getIntExtra(TOTAL_COUNT, 0)
-        // Generate the random number
-        val random = Random()
-        var randomInt = 0
-        // Add one because the bound is exclusive
-        if (count > 0) {
-            // Add one because the bound is exclusive
-            randomInt = random.nextInt(count + 1)
-        }
-        // Display the random number.
-        textview_random.text = Integer.toString(randomInt)
-        // Substitute the max value into the string resource
-        // for the heading, and update the heading
-        textview_label.text = getString(R.string.random_heading, count)
     }
 }
