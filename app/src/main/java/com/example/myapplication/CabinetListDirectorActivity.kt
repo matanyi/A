3f098e8918e6c0e29e18649f89f6ee182afd3910
Konstantin Razvodovskiy <hfprjc@gmail.com>
2020-07-15 14:03:02 +0700

    [+] Добавлен второй замок

--- app/src/main/java/com/example/myapplication/CabinetListDirectorActivity.kt
+++ app/src/main/java/com/example/myapplication/CabinetListDirectorActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.schoolscientistsexample.ServerCommand
+import kotlinx.android.synthetic.main.activity_cabinet_list_director.*
+import kotlinx.android.synthetic.main.activity_info.*
+
 
 class CabinetListDirectorActivity : AppCompatActivity() {
+
 
     var t = ServerCommand()
 
@@ -15,10 +19,30 @@ class CabinetListDirectorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cabinet_list_director)
     }
     fun lockOpen(view: View){
-        t.lockOpen()
+        var str =  t.lockOpen()
+        textView16.text = str
+        butt_op.text = "Открыто"
+        butt_cl.text = "Закрыть"
+
     }
     fun lockClose(view: View){
-        t.lockClose()
+        var str = t.lockClose()
+        textView16.text = str
+        butt_cl.text = "Закрыто"
+        butt_op.text = "Открыть"
+    }
+    fun lockOpen2(view: View){
+        var str2 =  t.lockOpen2()
+        textView18.text = str2
+        butt_op2.text = "Открыто"
+        butt_cl2.text = "Закрыть"
+
+    }
+    fun lockClose2(view: View){
+        var str2 = t.lockClose2()
+        textView18.text = str2
+        butt_cl2.text = "Закрыто"
+        butt_op2.text = "Открыть"
     }
     fun toMenu(view: View){
         val controlIntent = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/example/myapplication/ServerCommand.kt
+++ app/src/main/java/com/example/myapplication/ServerCommand.kt
@@ -2,38 +2,45 @@ package com.example.schoolscientistsexample
 
 import android.util.Log
 import io.ktor.client.HttpClient
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.http.renderSetCookieHeader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import io.ktor.client.features.logging.DEFAULT
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
+import kotlinx.coroutines.runBlocking
 
 
 class ServerCommand{
 
-    private val client = HttpClient() {
-        install(Logging) {
-            logger = Logger.DEFAULT
-            level = LogLevel.ALL
+    private val client = HttpClient()
+
+
+    private suspend fun lockOpenBody() : String {
+        try {
+            val res = client.get<String>("https://ms4.newtonbox.ru/lock/1/open")
+            Log.i("https://ms4.newtonbox.ru/lock/1/open Simple case", res)
+            return res
+        } catch (th: Throwable) {
+            return "ОШИБКА"
+        }
+    }
+    fun lockOpen() : String {
+        return runBlocking { lockOpenBody() }
+    }
+    private suspend fun lockCloseBody() : String {
+        try {
+            val res = client.get<String>("https://ms4.newtonbox.ru/lock/1/close")
+            Log.i("https://ms4.newtonbox.ru/lock/1/close Simple case", res)
+            return res
+        } catch (th: Throwable) {
+            return "ОШИБКА"
         }
     }
 
-
-    fun lockOpen() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val data = client.get<String>("https://ms4.newtonbox.ru/lock/1/open")
-            Log.i("https://ms4.newtonbox.ru/lock/1/open Simple case", data)
-        }
-
-    }
-    fun lockClose() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val data = client.get<String>("https://ms4.newtonbox.ru/lock/1/close")
-            Log.i("https://ms4.newtonbox.ru/lock/1/close Simple case", data)
-        }
+    fun lockClose() : String {
+        return runBlocking { lockCloseBody() }
     }
     fun lockStatus() {
         GlobalScope.launch(Dispatchers.IO) {
@@ -41,6 +48,33 @@ class ServerCommand{
             Log.i("https://ms4.newtonbox.ru/lock/1/status Simple case", data)
         }
     }
+    private suspend fun lockClose2Body() : String {
+        try {
+            val res2 = client.get<String>("https://ms4.newtonbox.ru/lock/2/close")
+            Log.i("https://ms4.newtonbox.ru/lock/2/close Simple case", res2)
+            return res2
+        } catch (th: Throwable) {
+            return "ОШИБКА"
+        }
+    }
+
+    fun lockClose2() : String {
+        return runBlocking { lockClose2Body() }
+    }
+    private suspend fun lockOpen2Body() : String {
+        try {
+            val res2 = client.get<String>("https://ms4.newtonbox.ru/lock/2/open")
+            Log.i("https://ms4.newtonbox.ru/lock/2/open Simple case", res2)
+            return res2
+        } catch (th: Throwable) {
+            return "ОШИБКА"
+        }
+    }
+
+    fun lockOpen2() : String {
+        return runBlocking { lockOpen2Body() }
+    }
+
     fun deviceList() {
         GlobalScope.launch(Dispatchers.IO) {
             val data = client.get<String>("https://ms0.newtonbox.ru/get/device_lock_list")
